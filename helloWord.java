0b246454c8592e61198d467f04d1f7994f286cbd
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-07-25 16:37:17 -0300

    first commit

---
+++ helloWord.java
@@ -0,0 +1,6 @@
+public class HelloWord { 
+    public static void main(String[] args){
+        System.out.println("Hello Word");
+    }
+}
+//void pode ser alterado conforme o tipo da variavel que está sendo utilizada;

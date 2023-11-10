fa0f18c264808bcd9343e3e4f76343df92533dfe
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-11-10 20:22:46 -0300

    Update Usuario.java

---
+++ relacao/Usuario.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Usuario {
+    public int id;
+    public String nome;
+    public String email;
+    public String password;
+
+    public static ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+
+    public CadastroAutor(int id, String nome, String email, String email, String password){
+    super(nome);
+    this.email = email;
+    this.nacionalidade = nacionalidade;
+    nomeAutores.add(this);
+    }
+}

a2b29802814af6eac4f0cee549eb7a49cdb1d93c
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-11-10 21:42:32 -0300

    Update Entrada.java

--- relacao/Entrada.java
+++ relacao/Entrada.java
@@ -1,12 +1,23 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+// import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+// import java.sql.Statement;
 
 public class Entrada {
-    public static void main(String[] args) {
+
+        private String nome;
+        private String email;
+        private String senha;
+
+        public Entrada(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+ 
+    public void inserirUsuario() {
         try {
             Connection connManager = DriverManager
                 .getConnection(
@@ -14,16 +25,16 @@ public class Entrada {
                     "root",
                     ""
                 );
-            System.out.println("Conexão estabelecida!");
-            PreparedStatement ps = connManager.prepareStatement("SELECT * FROM funcionario WHERE nome LIKE ?");
-            ps.setString(1, "%JA%");
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                System.out.println(rs.getString("nome"));
-            }
-            connManager.close();
+         System.out.println("Conexão estabelecida!"); //informa que a conexão com banco aconteceu certo
+         PreparedStatement ps = connManager.prepareStatement("insert into usuario.usuario values (?, ?, ?, ?)"); //comando que será realizado
+         ps.setLong(1, 0); //puxa o campo e o valor que será inserido
+         ps.setString(2, this.nome); 
+         ps.setString(3, this.email);
+         ps.setString(4, this.senha);
+         ps.executeUpdate(); //executa o comando
+         connManager.close(); //termina a conexão
         } catch (SQLException exception) {
-            System.out.println(exception.getMessage());
+            System.out.println(exception.getMessage()); //puxa uma mensagem de erro de conexão automático
         }
-    }
+    }  
 }

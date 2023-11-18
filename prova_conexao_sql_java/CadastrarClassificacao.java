5543f6be89c9c0ba9d56084bbf5c2dee3b9db8e4
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-11-17 21:53:16 -0300

    Update CadastrarClassificacao.java

--- prova_conexao_sql_java/CadastrarClassificacao.java
+++ prova_conexao_sql_java/CadastrarClassificacao.java
@@ -1,15 +1,27 @@
 package prova_conexao_sql_java;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CadastrarClassificacao {
     protected String classificacao;   
 
-    static ArrayList<CadastrarClassificacao> classificacoes = new ArrayList<CadastrarClassificacao>();
-
     //método construtor da classe pai
     public CadastrarClassificacao(String classificacao) {
         this.classificacao = classificacao;
-        classificacoes.add(this);
+        
+        try (Connection connManager = DriverManager.getConnection("jdbc:mysql://localhost:3306/prova_java_sql", "root", "")) {
+            try (PreparedStatement sc = connManager.prepareStatement("INSERT INTO prova_java_sql.cadastrarclassificacao VALUES (?, ?, ?, ?)")){ 
+                sc.setLong(1, 0); 
+                sc.setString(2, this.classificacao);
+                sc.executeUpdate();
+                System.out.println("Usuário cadastrado com sucesso!");
+            }
+        } catch (SQLException exception) {
+    System.out.println("Erro ao cadastrar usuário: " + exception.getMessage());
+}
     }
     //agrupamento de set's e get's
     public void setClassificacao(String classificacao){
@@ -22,10 +34,4 @@ public class CadastrarClassificacao {
     public String toString() {
         return "\n\tListagem de Classificação: " + this.classificacao;
     }    
-    //foreach para listar as classificações
-    public static void listarClassificacoes(){
-        for(CadastrarClassificacao listarClassificacao: classificacoes){
-            System.out.println("Classificações Cadastrados = "+listarClassificacao);
-        }
-    }
 }

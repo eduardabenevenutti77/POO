5b6ad4a07aebe76f01690a6bf8548233f102c2c0
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-11-19 17:30:25 -0300

    ...

--- prova_conexao_sql_java/CadastrarBiblioteca.java
+++ prova_conexao_sql_java/CadastrarBiblioteca.java
@@ -3,22 +3,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class CadastrarBiblioteca {
     private String nomeBiblioteca;
-    public  ArrayList<CadastroLivro> livros;
-    public  ArrayList<CadastrarEditora> editoras;
-    public ArrayList<CadastrarMidia> midias;
-    public ArrayList<CadastrarMidiaDigital> digitais;
 
     //método construtor da classe
     public CadastrarBiblioteca(String nomeBiblioteca){
         this.nomeBiblioteca = nomeBiblioteca;
-        this.livros = new ArrayList<CadastroLivro>();
-        this.midias = new ArrayList<CadastrarMidia>();
-        this.editoras = new ArrayList<CadastrarEditora>();
-        this.digitais = new ArrayList<CadastrarMidiaDigital>();
         
         try (Connection connManager = DriverManager.getConnection("jdbc:mysql://localhost:3306/prova_java_sql", "root", "")) {
             try (PreparedStatement sc = connManager.prepareStatement("INSERT INTO prova_java_sql.cadastrarbiblioteca VALUES (?, ?)")){ 
@@ -49,22 +40,5 @@ public class CadastrarBiblioteca {
     }
     public String getDigital(){
         return this.getDigital();
-    }
-    //incluido livro, mídia e mídia digital
-     public void incluirLivro(CadastroLivro livro) {
-        livros.add(livro);
-    }
-    public void incluirMidia(CadastrarMidia midia){
-        midias.add(midia);
-    }
-    public void incluirDigital(CadastrarMidiaDigital digital){
-        digitais.add(digital);
-    }
-    //método toString()
-    public String toString(){
-        return "\n\tListagem de Bibliotecas: \nNome da Biblioteca: "+
-        this.getNomeBiblioteca()+"\nNome do Livro: "+this.getTitulo()+
-        "\nNome da Editora"+this.getEmail()+"\nNome Mídia: "+this.getMidia()+
-        "& Mídia Digital: "+this.getDigital();
     }
 }
--- prova_conexao_sql_java/CadastrarClassificacao.java
+++ prova_conexao_sql_java/CadastrarClassificacao.java
@@ -3,7 +3,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class CadastrarClassificacao {
     protected String classificacao;   
